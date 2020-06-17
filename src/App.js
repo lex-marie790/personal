6e20aadd99daceca31df85a156dec5ac09fd489a
lex-marie790 <lexi.marie790@gmail.com>
2020-06-16 17:19:09 -0700

    homepage completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import styled from 'styled-components'
+// import styled from 'styled-components';
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+
 
 function App() {
   return (
     <Router>
-      <Nav>
-        <Link to='/'>Home</Link>
-        <Link to='/About'>About</Link>
-        <Link to='/Projects'>Projects</Link>
-        <Link to='/Contact'>Contact</Link>
-      </Nav>
+      <div className="nav">
+        <Link to='/' className='link'>Home</Link>
+        <Link to='/About' className='link'>About</Link>
+        <Link to='/Projects' className='link'>Projects</Link>
+        <Link to='/Contact' className='link'>Contact</Link>
+        <Link to='/Protected' className='link'>In Progress</Link>
+      </div>
     </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import MouseParticles  from 'react-mouse-particles'
+
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React"
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <MouseParticles g={1} color={["#ff75d6", "#bd60b4", "#a37ddf", "	#5aaee1", "#88c6ec"]} cull="col,image-wrapper"/>
+        <Router>
+          <div className="nav">
+            <Link to='/' className='link'>Home</Link>
+            <Link to='/About' className='link'>About</Link>
+            <Link to='/Projects' className='link'>Projects</Link>
+            <Link to='/Contact' className='link'>Contact</Link>
+            <Link to='/Protected' className='link'>In Progress</Link>
+          </div>
+        </Router>
+      </>
+    );
+  }
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
